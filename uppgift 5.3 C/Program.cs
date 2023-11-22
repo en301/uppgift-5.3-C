6a2b5ee882c3adb4d99f216a77d1f1adca8df395
en301 <emilsjostrand1000@gmail.com>
2023-11-22 10:31:40 +0100

    Add project files.

---
+++ uppgift 5.3 C/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uppgift_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = new string[4] {"Vad heter Love?", "Vad heter huvudstaden i Norge?", "Var kommer isak ifrån?", "Vad heter Markus?"};
+            string[] svar = new string[4] { "Love", "Oslo", "Sverige", "Markus" };
+
+            Console.WriteLine("Välj mellan fråga 1-4");
+            int tal = int.Parse(Console.ReadLine());
+            Console.WriteLine(frågor[tal-1]);
+            string frågesvar = Console.ReadLine();
+
+            if (frågesvar == svar[tal-1])
+            {
+                Console.WriteLine("Du hade rätt!!");
+            }
+            else
+            {
+                Console.WriteLine("Fel, rätt svar var " + svar[tal-1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
